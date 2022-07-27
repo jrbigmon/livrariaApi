ec2b5163dea4f36f4084b88981b9f40c829d1ff0
jrbigmon <junioterriaga0@gmail.com>
2022-07-27 15:18:26 -0300

    criado dotenv para segurança da aplicação e senhas de servidores

--- database/config.js
+++ database/config.js
@@ -1,11 +1,13 @@
+require('dotenv').config()
+
 module.exports = {
   "development": {
-    "username": "root",
-    "password": null,
-    "database": "livraria",
-    "host": "127.0.0.1",
-    "dialect": "mysql",
-    "port": 3306
+    "username": process.env.DB_USER,
+    "password": process.env.DB_PASS,
+    "database": process.env.DB_NAME,
+    "host": process.env.DB_HOST,
+    "dialect": process.env.DB_DIALECT,
+    "port": process.env.DB_PORT
   }
 }
 
